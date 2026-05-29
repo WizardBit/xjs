5271e6d6e33818908402852058e1b442e6f06684
Brandon Castillo <brandon.castillo@canonical.com>
2026-05-29 17:45:24 +0000

    Create License file

--- xjs/cli.py
+++ xjs/cli.py
@@ -1,19 +1,5 @@
 #!/usr/bin/env python3
-# This file is part of xjs a tool used to disply offline juju status
-# Copyright 2019 Canonical Ltd.
-#
-# This program is free software: you can redistribute it and/or modify it under
-# the terms of the GNU General Public License version 3, as published by the
-# Free Software Foundation.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranties of MERCHANTABILITY,
-# SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along with
-# this program.  If not, see <http://www.gnu.org/licenses/>.
-
+# SPDX-License-Identifier: GPL-3.0-only
 
 import json
 import sys
@@ -27,7 +13,6 @@ from .model import Model
 from .relation import Relation
 from prettytable import PrettyTable
 import yaml
-
 
 def load_status_file(inputfile, controllers):
     """Load a juju status file, inputfile is a yaml or json file"""
@@ -93,9 +78,9 @@ def load_status_file(inputfile, controllers):
     for appname, appinfo in rawstatus[applicationkey].items():
         if "relations" in appinfo:
             for relationname, partnerapps in appinfo["relations"].items():
-                for parnerapp in partnerapps:
+                for partnerapp in partnerapps:
                     relation = Relation(
-                        model, relationname, parnerapp, appname
+                        model, relationname, partnerapp, appname
                     )
                     model.add_relation(relation)
 
